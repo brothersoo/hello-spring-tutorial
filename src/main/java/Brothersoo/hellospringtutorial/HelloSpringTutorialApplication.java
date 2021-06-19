abb9496a623309e86dcdf8e680bcf337199e8257
Joseph Kim <Joseph.kim@travelflan.com>
2021-06-19 16:22:26 +0900

    init spring boot project

---
+++ src/main/java/Brothersoo/hellospringtutorial/HelloSpringTutorialApplication.java
@@ -0,0 +1,13 @@
+package Brothersoo.hellospringtutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloSpringTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloSpringTutorialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Brothersoo/hellospringtutorial/HelloSpringTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package Brothersoo.hellospringtutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloSpringTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
