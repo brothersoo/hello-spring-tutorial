5d7abab66585962d564ba0a77af89f424aee2744
brothersoo <moistybro@gmail.com>
2021-06-22 14:21:52 +0900

    ADD: field injection / setter injection example

--- src/main/java/Brothersoo/hellospringtutorial/controller/MemberController.java
+++ src/main/java/Brothersoo/hellospringtutorial/controller/MemberController.java
@@ -7,10 +7,35 @@ import org.springframework.stereotype.Controller;
 @Controller
 public class MemberController {
 
+  /**
+   * Constructor Injection
+   *
+   * Mostly recommended injection
+   */
   private final MemberService memberService;
 
   @Autowired
   public MemberController(MemberService memberService) {
     this.memberService = memberService;
   }
+
+
+  /**
+   * Field Injection
+   *
+   * Field injection is not recommended due to lack of scalability
+   */
+//  @Autowired private MemberService memberService;
+
+  /**
+   * Setter Injection
+   *
+   * Setter injection is also not recommended due to unnecessary public setter method
+   */
+//  private MemberService memberService;
+//
+//  @Autowired
+//  public void setMemberService(MemberService memberService) {
+//    this.memberService = memberService;
+//  }
 }
