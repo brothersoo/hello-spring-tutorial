c035c5561b60cedb8659f618212200bdd70d68df
brothersoo <moistybro@gmail.com>
2021-06-22 18:07:07 +0900

    MODIFY: member repository memory to simple jdbc

--- src/main/java/Brothersoo/hellospringtutorial/SpringConfig.java
+++ src/main/java/Brothersoo/hellospringtutorial/SpringConfig.java
@@ -1,21 +1,28 @@
 package Brothersoo.hellospringtutorial;
 
+import Brothersoo.hellospringtutorial.repository.JdbcMemberRepository;
 import Brothersoo.hellospringtutorial.repository.MemberRepository;
-import Brothersoo.hellospringtutorial.repository.MemoryMemberRepository;
 import Brothersoo.hellospringtutorial.service.MemberService;
+import javax.sql.DataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SpringConfig {
 
-  @Bean
-  public MemberRepository memberRepository() {
-    return new MemoryMemberRepository();
+  private final DataSource dataSource;
+  public SpringConfig(DataSource dataSource) {
+    this.dataSource = dataSource;
   }
 
   @Bean
-  public MemberService memberSerivce() {
+  public MemberService memberService() {
     return new MemberService(memberRepository());
+  }
+
+  @Bean
+  public MemberRepository memberRepository() {
+//    return new MemoryMemberRepository();
+    return new JdbcMemberRepository(dataSource);
   }
 }
